35eab0828750c800a6cec574abd285e6ff91c351
Hari Viapak Garg <hvg@Haris-MacBook-Air.local>
2021-09-12 19:37:28 +0530

    first commit

---
+++ map.py
@@ -0,0 +1,40 @@
+import folium
+import pandas
+
+df = pandas.read_csv("Volcanoes.txt")
+
+lat = list(df["LAT"])
+lon = list(df['LON'])
+elev = list(df['ELEV'])
+name = list(df['NAME'])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
+map = folium.Map(location= [38.58, -99.09] , zoom_start = 5, tiles= "OpenStreetMap")
+fgv = folium.FeatureGroup(name='Volcanoes')
+
+for lt, ln , el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    #fg.add_child(folium.Marker(location = [lt, ln],popup = folium.Popup(iframe), icon =folium.Icon(color=color_producer(el))))
+    fgv.add_child(folium.CircleMarker(location = [lt, ln],popup = folium.Popup(iframe), radius=6,
+    fill_color=color_producer(el), color='grey',fill_opacity=0.7 ))
+fgp = folium.FeatureGroup(name='Population')
+fgp.add_child(folium.GeoJson(data=open("world.json","r",encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']<10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'} ))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+map.save("Map_html_popup_simple.html")
